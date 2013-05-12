5b5089fc8c65dfac402ceaa95113e970fb6d5d2a
Kevin Wang <knwang@gmail.com>
2013-05-12 15:17:29 -0400

    Show recent videos in the UI

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@ Video.create(title: "Futurama", description: "space travel!", small_cover_url: "
 Video.create(title: "Monk", description: "Paranoid SF detective", small_cover_url: "/tmp/monk.jpg", category: dramas, large_cover_url: '/tmp/monk_large.jpg')
 Video.create(title: "Family Guy", description: "Peter Griffin and talking dog", small_cover_url: "/tmp/family_guy.jpg", category: comedies, large_cover_url: '/tmp/monk_large.jpg')
 Video.create(title: "South Park", description: "Hippie kids", small_cover_url: "/tmp/south_park.jpg", category: comedies, large_cover_url: '/tmp/monk_large.jpg')
+Video.create(title: "Futurama", description: "space travel!", small_cover_url: "/tmp/futurama.jpg", category: comedies, large_cover_url: '/tmp/monk_large.jpg')
+Video.create(title: "Monk", description: "Paranoid SF detective", small_cover_url: "/tmp/monk.jpg", category: dramas, large_cover_url: '/tmp/monk_large.jpg')
+Video.create(title: "Family Guy", description: "Peter Griffin and talking dog", small_cover_url: "/tmp/family_guy.jpg", category: comedies, large_cover_url: '/tmp/monk_large.jpg')
+Video.create(title: "South Park", description: "Hippie kids", small_cover_url: "/tmp/south_park.jpg", category: comedies, large_cover_url: '/tmp/monk_large.jpg')
+Video.create(title: "South Park", description: "Hippie kids", small_cover_url: "/tmp/south_park.jpg", category: comedies, large_cover_url: '/tmp/monk_large.jpg')
