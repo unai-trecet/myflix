cb8672d1324b71123bcb7f93226b63e536cbbce3
Kevin Wang <knwang@gmail.com>
2013-05-24 13:18:54 -0400

    User reorders queue

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Myflix::Application.routes.draw do
 
   resources :categories, only: [:show]
   resources :queue_items, only: [:create, :destroy]
+  post 'update_queue', to: 'queue_items#update_queue'
 
   get 'my_queue', to: 'queue_items#index'
-  resources :queue_items, only: [:create]
 
   get 'ui(/:action)', controller: 'ui'
   get 'register', to: "users#new"
