4f18852bc9c44bacf4939dabea41022447023215
Kevin Wang <knwang@gmail.com>
2013-06-01 16:33:48 -0400

    Remove extra migration

--- db/migrate/20130520022417_create_queue_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateQueueItems < ActiveRecord::Migration
-  def change
-    create_table :queue_items do |t|
-      t.integer :video_id, :user_id
-      t.integer :position
-      t.timestamps
-    end
-  end
-end
