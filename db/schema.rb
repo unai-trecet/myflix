6aa88f1a8fa3eaeaf52c3700035823dbba3ecab6
Kevin Wang <knwang@gmail.com>
2013-06-08 23:59:07 -0400

    User resets password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602224029) do
+ActiveRecord::Schema.define(:version => 20130607161312) do
 
   create_table "categories", :force => true do |t|
     t.string "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20130602224029) do
     t.string   "full_name"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "token"
   end
 
   create_table "videos", :force => true do |t|
