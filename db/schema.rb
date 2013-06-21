b57a82bc8368521e50985a0af70cede75496c91d
Kevin Wang <knwang@gmail.com>
2013-06-20 22:04:32 -0400

    Admin signs in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130618044845) do
+ActiveRecord::Schema.define(:version => 20130620202153) do
 
   create_table "categories", :force => true do |t|
     t.string "name"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20130618044845) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "token"
+    t.boolean  "admin"
   end
 
   create_table "videos", :force => true do |t|
