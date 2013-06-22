79bec32f553ddae679cf1681147f0e7c8009fb33
Kevin Wang <knwang@gmail.com>
2013-06-22 19:20:35 -0400

    Upload covers

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'bootstrap_form'
 gem 'bcrypt-ruby'
 gem 'fabrication'
 gem 'faker'
+gem 'carrierwave'
+gem 'mini_magick'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130620202153) do
+ActiveRecord::Schema.define(:version => 20130622040111) do
 
   create_table "categories", :force => true do |t|
     t.string "name"
@@ -63,12 +63,12 @@ ActiveRecord::Schema.define(:version => 20130620202153) do
 
   create_table "videos", :force => true do |t|
     t.string   "title"
-    t.string   "small_cover_url"
-    t.string   "large_cover_url"
     t.text     "description"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "category_id"
+    t.string   "large_cover"
+    t.string   "small_cover"
   end
 
 end
