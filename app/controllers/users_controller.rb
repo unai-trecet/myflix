3c5a571c7fd7f0d8cc9374a8aaba3286df78c85e
Kevin Wang <knwang@gmail.com>
2013-06-27 23:02:51 -0400

    Use stripe checkout to charge card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,13 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       handle_invitation
+      Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+      Stripe::Charge.create(
+        :amount => 999,
+        :currency => "usd",
+        :card => params[:stripeToken],
+        :description => "Sign up charge for #{@user.email}"
+      )
       AppMailer.send_welcome_email(@user).deliver
       redirect_to sign_in_path
     else
