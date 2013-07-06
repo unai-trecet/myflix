52ffc754c3ce595db61cad46fac1738a74fc866d
Kevin Wang <knwang@gmail.com>
2013-07-06 19:52:59 -0400

    Feature test for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,14 @@ class UsersController < ApplicationController
         @user.save
         handle_invitation
         AppMailer.send_welcome_email(@user).deliver
+        flash[:success] = "Thank you for registering with MyFlix. Please sign in now."
         redirect_to sign_in_path
       else
         flash[:error] = charge.error_message
         render :new
       end
     else
+      flash[:error] = "Invalid user information. Please check the errors below."
       render :new
     end
   end
