cf4da66745a4f3aa78f48b7404c34afff9a95b8a
Kevin Wang <knwang@gmail.com>
2013-08-16 06:11:42 -0400

    Subscription billing upon registration

--- spec/services/user_signup_spec.rb
+++ spec/services/user_signup_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe UserSignup do
   describe "#sign_up" do
     context "valid personal info and valid card" do
-      let(:charge) { double(:charge, successful?: true) }
+      let(:customer) { double(:customer, successful?: true) }
 
       before do
-        StripeWrapper::Charge.should_receive(:create).and_return(charge)
+        StripeWrapper::Customer.should_receive(:create).and_return(customer)
       end
 
       after do
@@ -54,8 +54,8 @@ describe UserSignup do
 
     context "valid personal info and declined card" do
       it "does not create a new user record" do
-        charge = double(:charge, successful?: false, error_message: "Your card was declined.")
-        StripeWrapper::Charge.should_receive(:create).and_return(charge)
+        customer = double(:customer, successful?: false, error_message: "Your card was declined.")
+        StripeWrapper::Customer.should_receive(:create).and_return(customer)
         UserSignup.new(Fabricate.build(:user)).sign_up('1231241', nil)
         expect(User.count).to eq(0)
       end
@@ -68,7 +68,7 @@ describe UserSignup do
       end
 
       it "does not charge the card" do
-        StripeWrapper::Charge.should_not_receive(:create)
+        StripeWrapper::Customer.should_not_receive(:create)
         UserSignup.new(User.new(email: "kevin@example.com")).sign_up('1231241', nil)
       end
 
